83c38800dfecf4c969ddb7308fbb74c6e5a09d55
lazyraven <nishayadav1703@gmail.com>
2025-07-27 14:59:54 +0530

    flower animation integrated for new learning practice 2025

---
+++ consoleTest.js
@@ -0,0 +1,11 @@
+//  Execute one by one all consoles..
+
+console.log(10 + 5 + "Nisha");
+console.log("Nisha" + 10 + 5);
+console.log("Nisha" - 10);
+console.log("Nisha" - 10 + 5);
+
+let obj1 = { x: 1 };
+let obj2 = { x: 1 };
+console.log(obj1 == obj2);
+console.log(obj1 === obj2);
